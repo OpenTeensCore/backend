31fdd9c37468426e9d0043c4ca1576fd544b2537
lanbinshijie <huoziye123@163.com>
2023-12-02 23:16:52 +0800

    Refactor API routes and add database initialization and heartbeat functionality

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, g
 from service import *
 import MercurySQLite
-
-db = MercurySQLite.DataBase('test.db')
 
 app = Flask(__name__)
 
@@ -10,13 +8,34 @@ app = Flask(__name__)
 def index():
     return 'Hello World!'
 
-@app.route('/api/v1.0/mercury', methods=['GET'])
+@app.route('/api/v1', methods=['GET'])
 def ping_heartbeat():
-    return jsonify({'status': 'OK'})
+    # return jsonify({'status': 'OK'})
+    return jsonify({'status': 'OK', 'code': 200, 'data': None})
 
-@app.route('/api/v1.0/mercury/<string:>', methods=['GET'])
-def ping_heartbeat():
-    return jsonify({'status': 'OK'})
+def get_db():
+    if 'db' not in g:
+        g.db = MercurySQLite.DataBase('test.db')
+        init_db(g.db)
+    return g.db
+
+@app.teardown_appcontext
+def close_db(error):
+    if 'db' in g:
+        del g.db
+        # g.db.close()
+
+@app.route('/api/v1/heartbeat', methods=['GET'])
+def heartbeat():
+    db = get_db()
+    result = do_heartbeat(db)
+    result = {'status': 'OK', 'code': 200, 'data': None}
+    return jsonify(result)
+
+# 没有匹配的路由
+@app.errorhandler(404)
+def page_not_found(error):
+    return jsonify({'status': 'ERROR', 'code': 404, 'message': 'Page not found'}), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
--- service.py
+++ service.py
@@ -1,1 +1,23 @@
 # 此处填写业务逻辑
+
+import MercurySQLite
+import time
+
+def init_db(db: MercurySQLite.DataBase):
+    # table = db['heartbeat']
+    # table.newColumn('id', int, primaryKey=True)
+    # # table.newColumn('time', str)
+    # print(table.columns)
+
+    # 初始化数据库的操作
+    ...
+
+def do_heartbeat(db: MercurySQLite.DataBase):
+    # 插入heartbeat新的数据
+    # table = db['heartbeat']
+    # table.insert(id=1)
+    # # 返回最新的数据
+    # return table[-1]
+
+    # 返回heartbeat的最新数据
+    ...
